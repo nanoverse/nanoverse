7c004555aad14394b9a0409e2c6e2d0ed47f05a3
David Bruce Borenstein <david.bruce.borenstein@gmail.com>
2015-07-27 12:44:47 -0400

    Symbol tables for output serializers. 1,325 tests (same four fail).

--- src/compiler/pipeline/translate/symbol/io/serialize/OutputClassSymbolTable.java
+++ src/compiler/pipeline/translate/symbol/io/serialize/OutputClassSymbolTable.java
@@ -26,6 +26,8 @@ package compiler.pipeline.translate.symbol.io.serialize;
 
 import compiler.pipeline.translate.symbol.*;
 import io.serialize.Serializer;
+import io.serialize.interactive.ProgressReporter;
+import io.serialize.text.*;
 
 import java.util.HashMap;
 import java.util.function.Supplier;
@@ -42,6 +44,105 @@ public class OutputClassSymbolTable extends ClassSymbolTable<Serializer> {
 
     @Override
     protected HashMap<String, Supplier<InstantiableSymbolTable>> resolveSubclasses() {
-        return null;
+        HashMap<String, Supplier<InstantiableSymbolTable>> ret = new HashMap<>();
+        cellStateWriter(ret);
+        haltTimeWriter(ret);
+        progressReporter(ret);
+        censusWriter(ret);
+        continuumHistogramWriter(ret);
+        surfaceCensusWriter(ret);
+        individualHaltWriter(ret);
+        interfaceCensusWriter(ret);
+        randomSeedWriter(ret);
+        runningTimeWriter(ret);
+        coordinateIndexer(ret);
+        continuumStateWriter(ret);
+        timeWriter(ret);
+        highlightWriter(ret);
+        visualizationSerializer(ret);
+        correlationWriter(ret);
+        return ret;
     }
+
+    public void cellStateWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = CellStateWriterInstSymbolTable::new;
+        ret.put("CellStateWriter", supplier);
+    }
+
+    public void haltTimeWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = HaltTimeWriterInstSymbolTable::new;
+        ret.put("HaltTimeWriter", supplier);
+    }
+
+    public void progressReporter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = ProgressReporterInstSymbolTable::new;
+        ret.put("ProgressReporter", supplier);
+    }
+
+    public void censusWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = CensusWriterInstSymbolTable::new;
+        ret.put("CensusWriter", supplier);
+    }
+
+    public void continuumHistogramWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = ContinuumHistoWriterInstSymbolTable::new;
+        ret.put("ContinuumHistogramWriter", supplier);
+    }
+
+    public void surfaceCensusWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = SurfaceCensusWriterInstSymbolTable::new;
+        ret.put("SurfaceCensusWriter", supplier);
+    }
+
+    public void individualHaltWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = IndividualHaltWriterInstSymbolTable::new;
+        ret.put("IndividualHaltWriter", supplier);
+    }
+
+    public void interfaceCensusWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = InterfaceCensusWriterInstSymbolTable::new;
+        ret.put("InterfaceCensusWriter", supplier);
+    }
+
+    public void randomSeedWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = RandomSeedWriterInstSymbolTable::new;
+        ret.put("RandomSeedWriter", supplier);
+    }
+
+    public void runningTimeWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = RunningTimeWriterInstSymbolTable::new;
+        ret.put("RunningTimeWriter", supplier);
+    }
+
+    public void coordinateIndexer(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = CoordinateIndexerInstSymbolTable::new;
+        ret.put("CoordinateIndexer", supplier);
+    }
+
+    public void continuumStateWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = ContinuumStateWriterInstSymbolTable::new;
+        ret.put("ContinuumStateWriter", supplier);
+    }
+
+    public void timeWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = TimeWriterInstSymbolTable::new;
+        ret.put("TimeWriter", supplier);
+    }
+
+    public void highlightWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = HighlightWriterInstSymbolTable::new;
+        ret.put("HighlightWriter", supplier);
+    }
+
+    public void visualizationSerializer(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = VisualizationSerializerInstSymbolTable::new;
+        ret.put("VisualizationSerializer", supplier);
+    }
+
+    public void correlationWriter(HashMap<String, Supplier<InstantiableSymbolTable>> ret) {
+        Supplier<InstantiableSymbolTable> supplier = CorrelationWriterInstSymbolTable::new;
+        ret.put("CorrelationWriter", supplier);
+    }
+
+
 }
