96e8cf863fc4cbad20a0b0afce5f57c8cb202b6c
David Bruce Borenstein <info@nanover.se>
2015-11-03 13:58:00 -0500

    Updated license:
    (1) Princeton University has waived its right to all aspects of the Nanoverse project and assigned these rights to David Bruce Borenstein. Documentation available upon request.
    
    (2) David Bruce Borenstein has granted shared ownership of the Nanoverse copyright to Nanoverse, LLC. David Bruce Borenstein also retains shared ownership of the Nanoverse copyright.
    
    (3) David Bruce Borenstein and Nanoverse, LLC agree to distribute Nanoverse according to the LGPL license.

--- src/nanoverse/runtime/agent/action/displacement/DisplacementOptionResolver.java
+++ src/nanoverse/runtime/agent/action/displacement/DisplacementOptionResolver.java
@@ -1,3 +1,23 @@
+/*
+ * Nanoverse: a declarative agent-based modeling language for natural and
+ * social science.
+ *
+ * Copyright (c) 2015 David Bruce Borenstein and Nanoverse, LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package nanoverse.runtime.agent.action.displacement;
 
 import nanoverse.runtime.agent.action.helper.CoordAgentMapper;
